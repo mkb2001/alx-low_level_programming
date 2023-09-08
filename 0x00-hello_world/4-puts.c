70ce2750192ec3e0dc19861bd3b9b8c2a21c77d6
Kevin Mugarura B <kevinmugarura051@gmail.com>
2023-09-08 09:47:57 -0700

    Hello

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,10 @@
 #include <stdio.h>
+
 /**
  * main - Entry point
  *
  * Return: Always 0 (Success)
  */
-
 int main(void)
 {
 	const char *str = "\"Programming is like building a multilingual puzzle";
