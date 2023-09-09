70cf00792659b1aa5d0dffe71032d580a52a6e79
Kevin Mugarura B <kevinmugarura051@gmail.com>
2023-09-09 05:22:53 -0700

    Hello world

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -12,9 +12,10 @@ int main(void)
 	long long veryLong;
 	float floatNumber;
 
-	printf("Size of a char: %lu bytes(s)\n", sizeof(character);
-	printf("Size of an int: %lu bytes(s)\n", sizeof(integer);
-	printf("Size of a long int: %lu bytes(s)\n", sizeof(longIntger);
-	printf("Size of a long long int: %lu bytes(s)\n", sizeof(veryLong);
-	printf("Size of a float: %lu bytes(s)\n", sizeof(floatNumber);
+	printf("Size of a char: %lu bytes(s)\n", sizeof(character));
+	printf("Size of an int: %lu bytes(s)\n", sizeof(integer));
+	printf("Size of a long int: %lu bytes(s)\n", sizeof(longIntger));
+	printf("Size of a long long int: %lu bytes(s)\n", sizeof(veryLong));
+	printf("Size of a float: %lu bytes(s)\n", sizeof(floatNumber));
+	return (0);
 }
