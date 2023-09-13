63da6b56e9e60f5f7242805735cfb5dbbd6bdc84
Kevin Mugarura B <kevinmugarura051@gmail.com>
2023-09-13 00:17:53 -0700

    x10

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * print_alphabet_x10 - check the code.
+ *
+ * Return: void
+ */
+void print_alphabet_x10(void)
+{
+	while (i<=10)
+	{
+		print_alphabet();
+		_putchar("\n");
+	}
+}
