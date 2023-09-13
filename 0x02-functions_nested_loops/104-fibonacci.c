8f075ff5ed368bc8dec8d355da18b0039848c4f4
Kevin Mugarura B <kevinmugarura051@gmail.com>
2023-09-13 02:56:00 -0700

    Advanced

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -5,46 +5,33 @@
  *
  * Return: Nothing
  */
-void add_str_nums(char *n1, char *n2, char *res) {
-    int carry = 0;
-    int len1 = strlen(n1);
-    int len2 = strlen(n2);
-    int max_len = len1 > len2 ? len1 : len2;
-    int i;
-    res[max_len + 1] = '\0';
+int main(void)
+{
+	unsigned long int fr1 = 0, bk1 = 1, fr2 = 0, bk2 = 2;
+	unsigned long int hold1, hold2, hold3;
+	int count;
 
-    for (i = 0; i < max_len; i++) {
-        int num1 = i < len1 ? n1[len1 - 1 - i] - '0' : 0;
-        int num2 = i < len2 ? n2[len2 - 1 - i] - '0' : 0;
-        int sum = num1 + num2 + carry;
-        res[max_len - i] = '0' + sum % 10;
-        carry = sum / 10;
-    }
-
-    if (carry > 0) {
-        res[0] = '0' + carry;
-    } else {
-        memmove(res, res + 1, max_len);
-    }
-}
-
-/**
- * main - check the code.
- *
- * Return: Always 0.
- */
-int main(void) {
-    char a[5000] = "1";
-    char b[5000] = "2";
-    char res[5000];
-    int i;
-    printf("%s, %s", a, b);
-    for ( i = 3; i <= 98; i++) {
-        add_str_nums(a, b, res);
-        printf(", %s", res);
-        strcpy(a, b);
-        strcpy(b, res);
-    }
-    printf("\n");
-    return (0);
+	printf("%lu, %lu, ", bk1, bk2);
+	for (count = 2; count < 98; count++)
+	{
+		if (bk1 + bk2 > LARGEST || fr2 > 0 || fr1 > 0)
+		{
+			hold1 = (bk1 + bk2) / LARGEST;
+			hold2 = (bk1 + bk2) % LARGEST;
+			hold3 = fr1 + fr2 + hold1;
+			fr1 = fr2, fr2 = hold3;
+			bk1 = bk2, bk2 = hold2;
+			printf("%lu%010lu", fr2, bk2);
+		}
+		else
+		{
+			hold2 = bk1 + bk2;
+			bk1 = bk2, bk2 = hold2;
+			printf("%lu", bk2);
+		}
+		if (count != 97)
+			printf(", ");
+	}
+	printf("\n");
+	return (0);
 }
