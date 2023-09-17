d04820550fce4ac74a2f1c950406915b1e6ec62d
Kevin Mugarura B <kevinmugarura051@gmail.com>
2023-09-16 23:27:04 -0700

    Hello

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,15 @@
+#include <main.h>
+/**
+ * _isdigit - check the code
+ * @c: digit to check
+ * Return: Always 0.
+ */
+int _isdigit(int c)
+{
+	if ( c >= 48  && c <= 57 )
+	{
+		return (1);
+	} 
+
+	return (0);
+}
