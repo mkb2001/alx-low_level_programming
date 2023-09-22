67c7261472dfd6634f9a5585d9367c214bf8c13e
Kevin Mugarura B <kevinmugarura051@gmail.com>
2023-09-21 23:08:38 -0700

    Pointers

---
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+  * print_square - Draws a square according parameter
+  * @size: The number of times to print square
+  *
+  * Return: empty
+  */
+
+void print_square(int size)
+{
+	for (i = 0; i <= size; i++)
+	{
+		for (j = 0;i<=size; j++)
+		{
+			_putchar(35);
+		}
+	}
+}
---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * main - reset_to_98 
+ * 
+ * Return: void
+ */
+
+void reset_to_98(int *n)
+{
+	*n = 98;	
+}
