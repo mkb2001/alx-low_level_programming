1f94e983f9060fb45799ddb4651deeb2b2d48530
Kevin Mugarura B <kevinmugarura051@gmail.com>
2023-09-26 22:41:47 -0700

    C

--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -3,3 +3,7 @@ void swap_int(int *a, int *b);
 void _puts(char *str);
 int _putchar(char c);
 void print_rev(char *s);
+char *_strcpy(char *dest, char *src);
+void print_array(int *a, int n);
+void puts2(char *str);
+void rev_string(char *s);
