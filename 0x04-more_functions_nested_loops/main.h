a0c05aa525dd0c17ca591b6ba274047dabd50e58
Kevin Mugarura B <kevinmugarura051@gmail.com>
2023-10-19 03:51:07 -0700

    Catchingup

--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -1,5 +1,7 @@
-#include <stdio.h>
+#ifndef FILE_MAIN
+#define FILE_MAIN
 
+int _putchar(char c);
 int _isupper(int c);
 int _isdigit(int c);
 int mul(int a, int b);
@@ -7,4 +9,9 @@ void print_numbers(void);
 void print_most_numbers(void);
 void more_numbers(void);
 void print_line(int n);
+void print_diagonal(int n);
 void print_square(int size);
+void print_triangle(int size);
+void print_number(int n);
+
+#endif
