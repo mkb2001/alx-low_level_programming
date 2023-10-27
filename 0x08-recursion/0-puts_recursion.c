03650189935fd7f74b385e292556605009125fd4
Kevin Mugarura <kevinmugarura051@gmail.com>
2023-10-27 18:41:08 +0300

    round

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -9,7 +9,13 @@
 
 void _puts_recursion(char *s)
 {
-    _putchar(s);
+    if (*s != '\0')
+    {
+        _putchar('\n');
+        return;
+    }
 
-    _putchar('\n');
+    _putchar(*s);
+    _puts_recursion(s + 1);
+
 }
