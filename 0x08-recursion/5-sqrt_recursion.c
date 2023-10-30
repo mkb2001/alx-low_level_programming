5c3ba9b0f0ba92b2e94694fd00dd43433bbb0feb
Kevin Mugarura <kevinmugarura051@gmail.com>
2023-10-30 09:33:29 +0300

    round

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -17,8 +17,11 @@ int _sqrt_recursion(int n)
 /*
 * actual_squareroot - function that returns the natural square root of a number.
 * @n: number to be evaluated
-* @i: number to be multiplied
+* @i: number to be multiplied by itself
+*
+* Return: natural square root of n
 */
+
 int actual_squareroot(int n, int i)
 {
 	if(i * i > n )
