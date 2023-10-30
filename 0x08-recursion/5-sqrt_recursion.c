8df98a59242c7293ac463a6d428f9b833040c3a3
Kevin Mugarura <kevinmugarura051@gmail.com>
2023-10-30 09:25:30 +0300

    round 'n round

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -10,15 +10,24 @@
 int _sqrt_recursion(int n)
 {
 	if (n < 0)
+		return (-1);
+	return (actual_squareroot(n, 0));
+}
+
+/*
+* actual_squareroot - function that returns the natural square root of a number.
+* @n: number to be evaluated
+* @i: number to be multiplied
+*/
+int actual_squareroot(int n, int i)
+{
+	if(i * i < 0 )
 	{
 		return (-1);
 	}
-	if (n > 0)
+	if (i * i == n)
 	{
-		return (n * _sqrt_recursion(n - 1));
+		return (i);
 	}
-	else
-	{
-		return (1);
-	}
+	return (actual_squareroot(n, i + 1));
 }
