7f6a16b7193bf412c4d7b376a1c1cb5ae51861a2
mkb2001 <kevinmugarura051@gmail.com>
2023-10-31 08:55:06 +0300

    round 'n round

--- 0x08-recursion/101-wildcmp.c
+++ 0x08-recursion/101-wildcmp.c
@@ -11,28 +11,13 @@
 
 int wildcmp(char *s1, char *s2)
 {
-	if (*s1 == '\0' && *s2 == '\0') /* if both strings are empty */
-	{
+	if (*s1 == '\0' && *s2 == '\0')
 		return (1);
-	}
-	if (*s1 == *s2) /* if characters match */
-	{
+	if (*s1 == *s2)
 		return (wildcmp(s1 + 1, s2 + 1));
-	}
-	if (*s2 == '*') /* if s2 is a wildcard */
-	{
-		if (*(s2 + 1) == '*') /* if s2 is a series of '*' */
-		{
-			return (wildcmp(s1, s2 + 1));
-		}
-		if (wildcmp(s1, s2 + 1)) /* if s2 is a wildcard */
-		{
-			return (1);
-		}
-		if (*(s1 + 1) != '\0' && wildcmp(s1 + 1, s2)) /* if s1 is not empty */
-		{
-			return (1);
-		}
-	}
+	if (*s2 == '*' && *(s2 + 1) != '\0' && *s1 == '\0')
+		return (0);
+	if (*s2 == '*')
+		return (wildcmp(s1, s2 + 1) || wildcmp(s1 + 1, s2));
 	return (0);
 }
