ee6545fc9fd37e4068d7141c25a05f92aeb178c6
mkb2001 <kevinmugarura051@gmail.com>
2023-10-31 17:28:28 +0300

    space

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -18,7 +18,7 @@ char *create_array(unsigned int size, char c)
 	array = malloc(sizeof(char) * size);
 	if (array == NULL)
 		return (NULL);
-	for (i = 0; i < 0; i++)
+	for (i = 0; i < size; i++)
 		array[i] = c;
 	return (array);
 }
