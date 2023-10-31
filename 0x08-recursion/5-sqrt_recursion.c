4c45ab77880f8a02cdf49efad408fb66b3ea80d1
mkb2001 <kevinmugarura051@gmail.com>
2023-10-31 08:31:26 +0300

    round 'n round

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -11,18 +11,18 @@ int _sqrt_recursion(int n)
 {
 	if (n < 0)
 		return (-1);
-	return (_actual_squareroot(n, 0));
+	return (actual_squareroot(n, 0));
 }
 
 /**
-* _actual_squareroot - function returns the natural square root of a number.
+* actual_squareroot - function returns the natural square root of a number.
 * @n: number to be evaluated
 * @i: number to be multiplied by itself
 *
 * Return: natural square root of n
 */
 
-int _actual_squareroot(int n, int i)
+int actual_squareroot(int n, int i)
 {
 	if (i * i > n)
 		return (-1);
