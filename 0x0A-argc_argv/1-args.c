37758a7fceb301dea6a3f16fae5a53c06240fd91
mkb2001 <kevinmugarura051@gmail.com>
2023-11-01 20:27:16 +0300

    args

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+* main - normal function
+* @argc: counter
+* @argv: array of pointers
+*
+* Return: Always Success(0)
+*/
+int main(int argc, char *argv[])
+{
+	printf("%d\n", argc);
+
+	return (0);
+}
