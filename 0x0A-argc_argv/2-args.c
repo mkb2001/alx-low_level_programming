569ef41bfcdafd6aacc0dbd4e88820c839b779fb
mkb2001 <kevinmugarura051@gmail.com>
2023-11-01 20:35:12 +0300

    args

---
+++ 0x0A-argc_argv/2-args.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+* main - nnormal function
+* @argc: counter
+* @argv: array
+*
+* Return: always success (0)
+*/
+
+int main(int argc, char *argv[])
+{
+	int i;
+
+	for (i = 0; i < argc; i++)
+	{
+		printf(argv[i]);
+	}
+
+	return (0);
+}
