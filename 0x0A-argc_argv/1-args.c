281874338fb31d7f37f34c61e9d38af82d16f6ba
mkb2001 <kevinmugarura051@gmail.com>
2023-11-01 20:29:44 +0300

    args

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include "main.h"
 /**
 * main - normal function
 * @argc: counter
@@ -6,9 +7,9 @@
 *
 * Return: Always Success(0)
 */
-int main(int argc, char *argv[])
+int main(int argc, char *argv[] __attribute__((unused)))
 {
-	printf("%d\n", argc);
+	printf("%d\n", argc - 1);
 
 	return (0);
 }
