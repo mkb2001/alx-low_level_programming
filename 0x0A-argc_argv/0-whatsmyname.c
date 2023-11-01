b18842742c2ca6196ee2be60ca84e48e61a9e250
mkb2001 <kevinmugarura051@gmail.com>
2023-11-01 20:15:46 +0300

    arguments

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+* main - it is a normal main file
+* @argc: it is the counter for the parameters
+* @argv: it is an array of pointers to the arguments
+*
+* Return: an integer
+*/
+int main(int argc, char *argv[])
+{
+	printf("%s", argv[0]);
+
+	return (0);
+}
