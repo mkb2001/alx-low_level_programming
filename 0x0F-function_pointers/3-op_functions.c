b008c7a93f845735310ec46c1d2c73c2ae33f587
mkb2001 <kevinmugarura051@gmail.com>
2023-11-14 10:41:30 +0300

    ops

---
+++ 0x0F-function_pointers/3-op_functions.c
@@ -0,0 +1,61 @@
+#include "3-calc.h"
+
+/**
+* op_add - adds numbers
+* @a: first number
+* @b: second number
+* Return: the sum
+*/
+
+int op_add(int a, int b)
+{
+	return (a + b);
+}
+
+/**
+* op_sub - subs numbers
+* @a: first number
+* @b: second number
+* Return: the difference
+*/
+
+int op_sub(int a, int b)
+{
+	return (a - b);
+}
+
+/**
+* op_mul - subs numbers
+* @a: first number
+* @b: second number
+* Return: the difference
+*/
+
+int op_mul(int a, int b)
+{
+	return (a * b);
+}
+
+/**
+* op_div - subs numbers
+* @a: first number
+* @b: second number
+* Return: the division
+*/
+
+int op_div(int a, int b)
+{
+	return (a / b);
+}
+
+/**
+* op_mod- subs numbers
+* @a: first number
+* @b: second number
+* Return: the difference
+*/
+
+int op_mod(int a, int b)
+{
+	return (a * b);
+}
