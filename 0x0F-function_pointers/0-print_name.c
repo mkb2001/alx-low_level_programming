4b5e8cfc11be2fc48a034c9357a0afbed0f7e0a1
mkb2001 <kevinmugarura051@gmail.com>
2023-11-14 09:57:06 +0300

    print name

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,16 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+* print_name - prints a name
+* @name: name to print
+* @f: pointer to function to use to print name
+*
+* Return: void
+*/
+
+void print_name(char *name, void (*f)(char *))
+{
+	if (name && f)
+		f(name);
+}
