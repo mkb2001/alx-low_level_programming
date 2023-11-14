cf83987763dbdd696c985f9ee6c7488f0f82a6cb
mkb2001 <kevinmugarura051@gmail.com>
2023-11-14 07:59:42 +0300

    dog_h

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,21 @@
+#ifndef DOG_H
+#define DOG_H
+/**
+ * struct dog - dog structure
+ * @name: name of dog
+ * @age: age of dog
+ * @owner: owner of dog
+ *
+ * Description: dog structure
+ */
+struct dog
+{
+	char *name;
+
+	float age;
+
+	char *owner;
+
+};
+
+#endif
