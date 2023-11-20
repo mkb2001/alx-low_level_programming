c31ad878d7d11f2711921e621f2417f4f8bb4d53
mkb2001 <kevinmugarura051@gmail.com>
2023-11-20 13:27:12 +0300

    linked list

---
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -0,0 +1,21 @@
+#include "lists.h"
+
+/**
+* print_listint  - function to find number
+* @h: this is the first node
+* Return: this returns a size
+*/
+
+size_t print_listint(const listint_t *h)
+{
+	size_t num;
+
+	while (h)
+	{
+		printf("%d\n", h->n);
+		num++;
+		h = h->next;
+	}
+
+	return (num);
+}
