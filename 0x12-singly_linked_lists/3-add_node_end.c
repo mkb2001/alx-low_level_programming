7bc98fc3babc344198f70b771f5bc22ce8ad420e
mkb2001 <kevinmugarura051@gmail.com>
2023-11-20 11:20:01 +0300

    linked

---
+++ 0x12-singly_linked_lists/3-add_node_end.c
@@ -0,0 +1,41 @@
+#include "lists.h"
+/**
+ * add_node_end - add new node to end of linked list
+ * @head: this is the head of the list
+ * @str: this is the string
+ * Return: the address of the new node.
+*/
+list_t *add_node_end(list_t **head, const char *str)
+{
+	list_t *new;
+	unsigned int count = 0;
+	list_t *temp = *head;
+
+	while (str[count])
+	{
+		count++;
+	}
+
+	new = malloc(sizeof(list_t));
+
+	if (!new)
+		return (NULL);
+
+	new->str = strdup(str);
+	new->len = count;
+	new->next = NULL;
+
+	if (*head == NULL)
+	{
+		*head = new;
+		return (new);
+	}
+
+	while (temp->next)
+		temp = temp->next;
+
+	temp->next = new;
+
+	return (new);
+}
+
