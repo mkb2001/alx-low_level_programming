36af8cc9a59f0af5d0b30dee5d5f18c7cb5e0a9a
mkb2001 <kevinmugarura051@gmail.com>
2023-11-23 16:11:17 +0300

    bits

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -8,15 +8,24 @@ unsigned int binary_to_uint(const char *b)
 {
 	unsigned int final, pow;
 
-	int length;
+	int length = 0;
 
 	if (b == NULL)
 		return (0);
 
-	for (length = 0; b[length]; length++)
+	/**
+	*for (length = 0; b[length]; length++)
+	*{
+	*	if (b[length] != '0' && b[length] != '1')
+	*		return (0);
+	*}
+	*/
+
+	while (b[length])
 	{
 		if (b[length] != '0' && b[length] != '1')
 			return (0);
+		length++;
 	}
 
 	for (pow = 1, final = 0, length--; length >= 0; length--, pow *= 2)
