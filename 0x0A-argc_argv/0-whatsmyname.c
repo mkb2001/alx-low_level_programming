95e2da5c948309bb27bee2b4f5d098ae5724741d
mkb2001 <kevinmugarura051@gmail.com>
2024-01-21 13:00:05 +0300

    argument 0

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+ * main - prints program name
+ * @argv: pointer to array of strings
+ * @argc: the number of arguments
+ * Return: the program name
+*/
+
+int main(int argc, char *argv[])
+{
+	printf("%s\n", argv[0]);
+
+	return (0);
+}
