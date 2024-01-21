e6aa8bd970185151d01b5543c3fd47eee78c01d5
mkb2001 <kevinmugarura051@gmail.com>
2024-01-22 00:42:59 +0300

    dupe

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,40 @@
+#include "main.h"
+#include <stdlib.h>
+/**
+* _strdup - creates a pointer to a newly allocated space in memory,
+* which contains a copy of the string given as a parameter.
+* @str: the string to be copied
+* Return: NULL if str = NULL
+*/
+
+char *_strdup(char *str)
+{
+	int i, len;
+
+	char *mem;
+
+
+	len = 0;
+	i = 0;
+
+	if (str == NULL)
+		return (NULL);
+
+	while (str[i] != '\0')
+		i++;
+
+	mem = malloc(sizeof(char) * i);
+
+	if (mem == NULL)
+		return (0);
+
+	while (str[len] != '\0')
+	{
+		*(mem + len) = str[len];
+		len++;
+	}
+	*(mem + len) = '\0';
+
+	return (mem);
+
+}
--- 0x0B-malloc_free/main.h
+++ 0x0B-malloc_free/main.h
@@ -1,5 +1,6 @@
 #ifndef MAIN_FILE
 #define MAIN_FILE
 char *create_array(unsigned int size, char c);
-
+char *_strdup(char *str);
+char *str_concat(char *s1, char *s2);
 #endif
