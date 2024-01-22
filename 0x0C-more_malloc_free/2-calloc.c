8b81b5b50e1ff05fa2558b839921a7d5b6521a23
mkb2001 <kevinmugarura051@gmail.com>
2024-01-22 14:26:25 +0300

    unused

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -30,9 +30,7 @@ char *_memset(char *s, char b, unsigned int n)
 void *_calloc(unsigned int nmemb, unsigned int size)
 {
 	void *arr;
-
-	unsigned int i = 0;
-
+	
 	if (nmemb == 0 || size == 0)
 		return (NULL);
 
