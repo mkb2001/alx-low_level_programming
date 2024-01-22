eece0dfb29b6e0f4188f1b1d5c732ed464e27110
mkb2001 <kevinmugarura051@gmail.com>
2024-01-22 15:46:25 +0300

    library

--- 0x0C-more_malloc_free/100-realloc.c
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -1,5 +1,6 @@
 #include "main.h"
-
+#include <stdio.h>
+#include <stdlib.h>
 /**
  * _realloc - a function that reallocates a memory block using malloc and free
  * @ptr: is a pointer to the memory previously allocated with a call to malloc
