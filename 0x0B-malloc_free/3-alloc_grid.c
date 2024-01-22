3e5082aa58ae1ac211bb219bf58fc10930f0650d
mkb2001 <kevinmugarura051@gmail.com>
2024-01-22 10:43:22 +0300

    alloc

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -17,7 +17,7 @@ int **alloc_grid(int width, int height)
 	if (width <= 0 || height <= 0)
 		return (NULL);
 
-	p = (int **)malloc((sizeof(int) * (height + width + 1)));
+	p = (int **)malloc((sizeof(int *) * height));
 
 	if (p == NULL)
 		return (0);
