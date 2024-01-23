16757bfb3b65df5dfa683045afe51db642e531e7
mkb2001 <kevinmugarura051@gmail.com>
2024-01-24 02:41:18 +0300

    Refactor print_dog function to handle NULL values

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -1,31 +1,19 @@
 #include "dog.h"
 #include <stdio.h>
 /**
-*
+* print_dog - prints a struct dog
+* @d: struct dog to print
 */
 
 void print_dog(struct dog *d)
 {
-if (d == NULL)
-	printf(" ");
+	if (d == NULL)
+		return;
 
-if (d->name)
-    printf("Name: %s\n", d->name);
+	if (d->name == NULL)
+		d->name = "(nil)";
+	if (d->owner == NULL)
+		d->owner = "(nil)";
 
-else
-    printf("Name: (nil)\n");
-
-if (d->age)
-    printf("Age: %f\n", d->age);
-
-else
-    printf("Age: (nil)\n");
-
-if (d->owner)
-    printf("Owner: %s\n", d->owner);
-
-else
-    printf("Name: (nil)\n");
-
-
+	printf("Name: %s\nAge: %f\nOwner: %s\n", d->name, d->age, d->owner);
 }
