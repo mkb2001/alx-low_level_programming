a39625600c4e44ae3ee54d8a1a9394d205f74847
mkb2001 <kevinmugarura051@gmail.com>
2024-01-24 02:11:55 +0300

    Add structures and dog.h file

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,21 @@
+#ifndef OBJ_FILE_1
+#define OBJ_FILE_1
+/**
+* struct dog - structure about a dog
+* @name: the name of a dog
+* @age: the age of a dog
+* @owner: the dog's owner
+*
+* Description - Define a new type struct dog with the following elements:
+*/
+struct dog
+{
+	char *name;
+
+	char *owner;
+
+	float age;
+
+}
+
+#endif
