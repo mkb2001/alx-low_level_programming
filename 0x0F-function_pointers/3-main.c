06390f65030b8590a5d3598efcce3ddb4eead0de
mkb2001 <kevinmugarura051@gmail.com>
2024-01-25 15:14:04 +0300

    Fix error handling in main function

--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -29,14 +29,14 @@ int main(int argc, char *argv[])
 	if (!func)
 	{
 		printf("Error\n");
-		exit(98);
+		exit(99);
 	}
 	o = *argv[2];
 
 	if ((o == '/' || o == '%') && arg2 == 0)
 	{
 		printf("Error\n");
-		exit(98);
+		exit(100);
 	}
 
 	result = func(arg1, arg2);
