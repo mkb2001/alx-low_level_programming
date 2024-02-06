6829c528179aaeea7dd59df86204f886553b9a05
mkb2001 <kevinmugarura051@gmail.com>
2024-02-06 17:53:11 +0300

    Linked Lists

---
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -0,0 +1,29 @@
+#include "lists.h"
+/**
+ * print_list - This prints the linked list
+ * @h: the list
+ * Return: The number of nodes
+*/
+size_t print_list(const list_t *h)
+{
+	size_t i;
+
+	i = 0;
+
+	if (h == NULL)
+		printf("[0] (nil)");
+
+	while (h->next != NULL)
+    {
+		if (h->str == NULL)
+			printf("[0] (nil)");
+
+		i++;
+
+    }
+
+
+	return (i);
+}
+
+
