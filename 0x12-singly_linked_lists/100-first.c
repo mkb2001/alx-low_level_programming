165fdf7b53561efbfc18191516b43938c4cf7df0
mkb2001 <kevinmugarura051@gmail.com>
2024-02-06 23:35:53 +0300

    Fix function attribute in 100-first.c and update main.c

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 
-void first_to_print(void) _attribute__((constructor));
+void first_to_print(void) __attribute__((constructor));
 
 /**
 * first_to_print - prints a sentence before the main function is executed.
