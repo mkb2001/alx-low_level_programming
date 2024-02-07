b64c9ae87641a2d42ad78ebafcd7e5914e8f3d57
mkb2001 <kevinmugarura051@gmail.com>
2024-02-07 11:34:54 +0300

    Add free_listint2 function

---
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -0,0 +1,17 @@
+#include "lists.h"
+/**
+* free_listint2 -  function that frees a listint_t list.
+* @head: The first node;
+*/
+void free_listint2(listint_t **head)
+{
+	listint_t *tmp;
+
+	while ((*head))
+	{
+		tmp = (*head)->next;
+		free((*head));
+		(*head) = tmp;
+	}
+
+}
