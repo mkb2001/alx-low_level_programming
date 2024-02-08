3ab97a2673273a6f7913d6cc41dac0d078bf83f9
mkb2001 <kevinmugarura051@gmail.com>
2024-02-08 16:49:48 +0300

    Refactor print_listint_safe function

--- 0x13-more_singly_linked_lists/101-print_listint_safe.c
+++ 0x13-more_singly_linked_lists/101-print_listint_safe.c
@@ -9,41 +9,39 @@
 size_t print_listint_safe(const listint_t *head)
 {
 
-    const listint_t *temp = NULL;
-    const listint_t *l = NULL;
+	const listint_t *temp = NULL;
+	const listint_t *l = NULL;
 
 
-    size_t new;
-    size_t i = 0;
+	size_t new;
+	size_t i = 0;
 
-    temp = head;
+	temp = head;
 
-    while(temp)
-    {
-        printf("[%p] %d\n", (void *)temp, temp->n);
-        i++;
-        temp = temp->next;
-        l = head;
-        new = 0;
-        while (new < i)
-        {
-            if (temp == l)
-            {
-                printf("-> [%p] %d\n", (void *)temp, temp->n);
-                return (i);
+	while (temp)
+	{
+		printf("[%p] %d\n", (void *)temp, temp->n);
+		i++;
+		temp = temp->next;
+		l = head;
+		new = 0;
+		while (new < i)
+		{
+			if (temp == l)
+			{
+				printf("-> [%p] %d\n", (void *)temp, temp->n);
+				return (i);
 
-            }
-            l  = l->next;
-            new++;
-        }
-        if (!head)
-        {
-            exit(98);
+			}
+			l  = l->next;
+			new++;
+		}
+		if (!head)
+		{
+			exit(98);
+		}
 
-        }
-        
-        
-    }
-    return (i);
+	}
+	return (i);
 
 }
