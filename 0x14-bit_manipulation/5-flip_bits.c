107f792f6e6981efd8bef967091a6a9158018a7d
mkb2001 <kevinmugarura051@gmail.com>
2024-02-08 23:46:30 +0300

    Add clear_bit function and flip_bits function

---
+++ 0x14-bit_manipulation/5-flip_bits.c
@@ -0,0 +1,18 @@
+/**
+ * flip_bits - the number of bits you would need to flip to get
+ *  from one number to another.
+ * @n: the first number
+ * @m: the second number
+ * Return: the number of bits needed
+*/
+
+unsigned int flip_bits(unsigned long int n, unsigned long int m)
+{
+    unsigned long int xor_result = n ^ m;
+    unsigned int count = 0;
+
+    while (xor_result)
+    {
+        count += xor_result
+    }
+}
