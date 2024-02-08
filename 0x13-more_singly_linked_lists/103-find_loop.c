fb63a5a033104d8f2c06d7541b20ed6f69340a3f
mkb2001 <kevinmugarura051@gmail.com>
2024-02-08 17:23:15 +0300

    Fix loop detection in find_listint_loop function

--- 0x13-more_singly_linked_lists/103-find_loop.c
+++ 0x13-more_singly_linked_lists/103-find_loop.c
@@ -1,35 +1,38 @@
 #include "lists.h"
 /**
- * find_listint_loop -  a function that finds the loop in a linked list.
+* find_listint_loop -  a function that finds the loop in a linked list.
+* @head: This is the first node.
+* Return: The address of the node where the loop starts,
+*  or NULL if there is no loop
 */
 
 listint_t *find_listint_loop(listint_t *head)
 {
-    listint_t *fast = head;
+	listint_t *fast = head;
 
-    listint_t *slow = head;
+	listint_t *slow = head;
 
-    if (!head)
-    {
-        return (NULL);
-    }
+	if (!head)
+	{
+		return (NULL);
+	}
 
-    while (slow && fast && fast->next)
-    {
-        slow = slow->next;
-        fast = fast->next->next;
-        if (slow == fast)
-        {
-            slow = head;
-            while (slow != fast)
-            {
-                slow = slow->next;
-                fast = fast->next;
+	while (slow && fast && fast->next)
+	{
+		slow = slow->next;
+		fast = fast->next->next;
+		if (slow == fast)
+		{
+			slow = head;
+			while (slow != fast)
+			{
+				slow = slow->next;
+				fast = fast->next;
 
-            }
-            return (fast);
-        }
+			}
+			return (fast);
+		}
 
-    }
-    return (NULL);
+	}
+	return (NULL);
 }
