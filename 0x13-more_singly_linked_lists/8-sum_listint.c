98883db7a723f2718bb383655c64cf241be929c3
mkb2001 <kevinmugarura051@gmail.com>
2024-02-08 11:07:21 +0300

    Refactor sum_listint function to handle empty list case

--- 0x13-more_singly_linked_lists/8-sum_listint.c
+++ 0x13-more_singly_linked_lists/8-sum_listint.c
@@ -1,14 +1,22 @@
 #include "lists.h"
-
+/**
+* sum_listint - The  returns the sum of all the data (n)
+* of a listint_t linked list.
+* @head: The first node
+* Return: The sum
+*/
 int sum_listint(listint_t *head)
 {
-    int i = 0;
-    listint_t *tmp = head;
+	if (!head)
+		return (0);
+	int i = 0;
 
-    while(tmp)
-    {
-        i += tmp->n;
-        tmp = tmp->next;
-    }
-    return (i);
+	listint_t *tmp = head;
+
+	while (tmp)
+	{
+		i += tmp->n;
+		tmp = tmp->next;
+	}
+	return (i);
 }
