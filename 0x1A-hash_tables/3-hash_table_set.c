fd0c903660a4269a7f9eb9fd1794110fd7101ca7
mkb2001 <kevinmugarura051@gmail.com>
2024-04-15 00:18:45 +0300

    Add betty changes

---
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -0,0 +1,14 @@
+#include "hash_tables.h"
+/**
+* hash_table_set
+* @param ht
+* @param key
+* @return
+*/
+
+int hash_table_set(hash_table_t *ht, const char *key, const char *value)
+{
+	if (key == NULL)
+		return
+
+}
